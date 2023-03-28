7c3c906077bd00ea8c5fdb53de69438a4b4f61e0
ThibautMilville <tmilville.pro@gmail.com>
2023-03-28 23:02:29 +0200

    PHP + SQL

--- ajouter.php
+++ ajouter.php
@@ -8,10 +8,41 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php
+        // VERIFY IF THE BUTTON HAS BEEN CLICKED
+        if(isset($_POST['button'])){
+            // EXTRACT THE VARIABLES
+            extract($_POST);
+            // VERIFY IF ALL THE FIELDS ARE FILLED
+            if(!empty($nom) && !empty($prenom) && !empty($age)){
+                // DATABASE CONNECTION
+                include_once("connexion.php");
+                // INSERT QUERY
+                $req = mysqli_query($con, "INSERT INTO employe VALUES (NULL, '$nom', '$prenom', '$age')");
+                // CLOSE THE CONNECTION
+                mysqli_close($con);
+                // IF REQUEST SUCCESS -> REDIRECTION TO THE INDEX PAGE
+                if($req){
+                    header("location:index.php");
+                }else{
+                    $message = "Employé non ajouté";
+                }
+            }else{
+                $message = "Veuillez remplir tous les champs !";
+            }
+        }
+    ?>
     <div class="form">
         <a href="index.php" class="back_btn"><img src="images/back.png">Retour</a>
         <h2>Ajouter un employé</h2>
-        <p class="erreur_message">Veuillez remplir tous les champs !</p>
+        <p class="erreur_message">
+            <?php
+                // IF THE VARIABLE EXIST -> DISPLAY THE ERROR MESSAGE
+                if(isset($message)){
+                    echo $message;
+                }
+            ?>
+        </p>
         <form action="" method="POST">
             <label>Nom</label>
             <input type="text" name="nom">
